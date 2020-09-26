d583f95f1751ce714dfb9aacf2a8ce871d8b73c8
gyc <892466942@qq.com>
2020-09-26 22:26:46 +0800

    fux bug

--- pack.js
+++ pack.js
@@ -7,8 +7,15 @@ function execute(cmd) {
 
 
 function build_game() {
+    // 编译项目
     execute(`/Applications/CocosCreator/Creator/2.4.2/CocosCreator.app/Contents/MacOS/CocosCreator --path ./ --build "platform=android;apiLevel=30;apilevel=30;debug=true;previewWidth=720;previewHeight=1280"`)
     console.log("build game succeed.");
+
+    // 加密图片资源
+    const entry_png = "python en.py 1 2 3";
+    execute(entry_png);
+
+    // // 打包APK
     const pack_apk = "cd ./build/jsb-link/frameworks/runtime-src/proj.android-studio && ./gradlew assembleDebug";
     execute(pack_apk);
 }
